81e5ca3113faa8514eafc3daa701d4b41131712b
Роман Носов <romannosov_2001@mail.ru>
2022-10-17 19:59:24 +0300

    Add Class Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class Calculator {
+
+    static Supplier<Calculator> instance = Calculator::new;
+
+    ref = Calculator::new;
+
+
+}
