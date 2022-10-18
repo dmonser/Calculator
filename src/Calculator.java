a8c3142be52927dd167afefa13c3c6f7ee776be9
Роман Носов <romannosov_2001@mail.ru>
2022-10-18 20:28:01 +0300

    Final version

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,25 @@
-import java.util.function.Consumer;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class Calculator {
 
     static Supplier<Calculator> instance = Calculator::new;
 
-    ref = Calculator::new;
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> {
+        try {
+            return x / y;
+        } catch (ArithmeticException e) {
+            System.out.println("No zero division");
+        }
+        return null;
+    }; //При записи вида "(x, y) -> x / y" мы получили деление на 0.
 
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
 
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
 }
