42d2df07aa2ccf9e43817fbf7f472a7f511195c0
Роман Носов <romannosov_2001@mail.ru>
2022-10-31 17:47:33 +0300

    Added check on zero division

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,12 +8,11 @@ public class Calculator {
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
     BinaryOperator<Integer> devide = (x, y) -> {
-        try {
-            return x / y;
-        } catch (ArithmeticException e) {
+        if (y == 0) {
             System.out.println("No zero division");
+            return null;
         }
-        return null;
+        return x / y;
     }; //При записи вида "(x, y) -> x / y" мы получили деление на 0.
 
     UnaryOperator<Integer> pow = x -> x * x;
